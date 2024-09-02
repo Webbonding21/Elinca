b35cbfb5bb51d60f09384cb60c644a0b64002ae3
Webbonding21 <webbonding21@gmail.com>
2024-09-02 00:09:42 -0400

    Actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Header from './components/Header';
 import Hero from './components/Hero';
 import Section from './components/Section';
 import '../src/css/App.css';
